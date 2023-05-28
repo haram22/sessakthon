c78b3bf49da30f32b0ad5079dd039ba05c941a0b
dusagong <yoonseungjae123@gmail.com>
2023-05-28 16:22:11 +0900

    color Changing

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'sessakthon🌱',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 34, 4, 85)),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 90, 7, 233)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
