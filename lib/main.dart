60b9dd846ec35add4529dc3bf2d6137c6848ec2f
jinseo-hgu <eunoia@handong.ac.kr>
2023-05-29 22:19:21 +0900

    image.network에서 asset.image로 바꾸기

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_wallpaper_manager/flutter_wallpaper_manager.dart';
-import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter_gif/flutter_gif.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +27,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
@@ -32,31 +35,38 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var imageUrl =
-      "https://mblogthumb-phinf.pstatic.net/MjAxNzEyMTNfMTcx/MDAxNTEzMTQ4OTc3OTQx.8ZWyFPYKvp48d-qHLL4qBMAIO6RKp60XYJjtYnr9HSkg.yKxlMfuNsxyQ5ApWgmuunyPNfK7z5sGQaOYAek-ZXygg.JPEG.mypetparty/adam-krowitz-386792.jpg?type=w800";
-
   @override
   Widget build(BuildContext context) {
+    Image image = Image(image: AssetImage('assets/rocket.gif'));
+    fetchGif(AssetImage("assets/rocket.gif"));
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("set Image"),
+        title: const Text("set Image"),
       ),
-      body: Column(children: [
-        Image.network(
-          imageUrl,
-          fit: BoxFit.cover,
-          height: 500,
-        )
-      ]),
+      body: Column(
+        children: [
+          image,
+        ],
+      ),
       floatingActionButton: FloatingActionButton.extended(
-          onPressed: () async {
-            int location =
-                WallpaperManager.LOCK_SCREEN; //can be Home/Lock Screen
-            var file = await DefaultCacheManager().getSingleFile(imageUrl);
-            bool result = await WallpaperManager.setWallpaperFromFile(
-                file.path, location);
-          },
-          label: Text("Set 🌱")),
+        onPressed: () async {
+          int location = WallpaperManager.BOTH_SCREEN;
+          await setWallpaperFromAsset('assets/rocket.gif', location);
+        },
+        label: const Text("Set 🌱"),
+      ),
     );
+  }
+
+  Future<void> setWallpaperFromAsset(String assetPath, int location) async {
+    String fileName = assetPath.split('/').last;
+    String filePath = (await getTemporaryDirectory()).path + '/$fileName';
+
+    ByteData assetData = await rootBundle.load(assetPath);
+    List<int> bytes = assetData.buffer.asUint8List();
+    await File(filePath).writeAsBytes(bytes);
+
+    await WallpaperManager.setWallpaperFromFile(filePath, location);
   }
 }
