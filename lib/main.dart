645c5c3155d3e91cab5cececda845dcc0e61ced9
jinseo-hgu <eunoia@handong.ac.kr>
2023-06-07 00:23:34 +0900

    pr을 위한 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:screen_state/screen_state.dart';
 import 'Controller/unLockCount.dart';
 
+// splash
 enum ScreenStateEvent { SCREEN_UNLOCKED, SCREEN_ON, SCREEN_OFF }
 
 void main() {
