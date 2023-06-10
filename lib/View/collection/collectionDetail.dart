998b1b542664bf27ad348169cf916e32ec726669
dusagong <yoonseungjae123@gmail.com>
2023-06-11 00:24:03 +0900

    ..

--- lib/View/collection/collectionDetail.dart
+++ lib/View/collection/collectionDetail.dart
@@ -12,16 +12,92 @@ void showAlertDialog(BuildContext context, CollectionItem item,
       return AlertDialog(
         backgroundColor: const Color(0xff242424),
         contentPadding: EdgeInsets.zero,
-        content: SingleChildScrollView(
-          child: Column(
-            children: [
-              Stack(
+        content: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Column(
                 children: [
                   AspectRatio(
                     aspectRatio: controller.value.aspectRatio,
                     child: VideoPlayer(controller),
                   ),
-                  Padding(
+                  const SizedBox(height: 30),
+                  Text(
+                    item.tier,
+                    style: title2(color: item.color),
+                  ),
+                  Image.asset(
+                    item.image,
+                    width: 200,
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(height: 50),
+                  Text(
+                    item.name,
+                    style: title1(),
+                  ),
+                  const SizedBox(height: 5),
+                  const Text(
+                    "#즉석밥 #오뚜기밥",
+                    style: TextStyle(
+                      fontFamily: 'gmarket',
+                      fontWeight: FontWeight.w100,
+                      fontSize: 15,
+                    ),
+                  ),
+                  const SizedBox(height: 60),
+                  const Row(
+                    children: [
+                      SizedBox(width: 20),
+                      Text(
+                        "분리배출 방법",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontFamily: 'gmarket',
+                          fontWeight: FontWeight.w500,
+                          fontSize: 15,
+                          color: mainColor_green,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    "- 빈 용기는 밥알이 남아있지 않게 깨끗이 세척한 후\n   플라스틱으로 배출해요.\n- 비닐 뚜껑은 떼어내서 비닐류로 배출해요.",
+                    style: subtitle1(),
+                  ),
+                  const SizedBox(height: 20),
+                  item.isCollected
+                      ? Column(
+                          children: [
+                            const Row(
+                              children: [
+                                SizedBox(width: 20),
+                                Text(
+                                  "알아두면 좋은 점",
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    fontFamily: 'gmarket',
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 15,
+                                    color: mainColor_green,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              "즉석밥 용기에 쓰이는 플라스틱 OTHER는\n내열성을 높이기 위해 여러 재질을 혼합한 복합\n플라스틱으로 제품 종류별로 수거해 별도의\n재활용 공정을 거치지 않는 이상 재활용이 불가능해요.\n그래서 재활용장에서 선별되지 못하고 대부분\n일반쓰레기로 폐기(소각 또는 매립)된다고 합니다.",
+                              style: subtitle1(),
+                            ),
+                          ],
+                        )
+                      : const SizedBox(height: 115),
+                ],
+              ),
+            ),
+            Padding(
                     padding: const EdgeInsets.only(
                       top: 20,
                       left: 20,
@@ -31,83 +107,7 @@ void showAlertDialog(BuildContext context, CollectionItem item,
                       onTap: () => Navigator.pop(context),
                     ),
                   ),
-                ],
-              ),
-              const SizedBox(height: 30),
-              Text(
-                item.tier,
-                style: title2(color: item.color),
-              ),
-              Image.asset(
-                item.image,
-                width: 200,
-                height: 200,
-                fit: BoxFit.cover,
-              ),
-              const SizedBox(height: 50),
-              Text(
-                item.name,
-                style: title1(),
-              ),
-              const SizedBox(height: 5),
-              const Text(
-                "#즉석밥 #오뚜기밥",
-                style: TextStyle(
-                  fontFamily: 'gmarket',
-                  fontWeight: FontWeight.w100,
-                  fontSize: 15,
-                ),
-              ),
-              const SizedBox(height: 60),
-              const Row(
-                children: [
-                  SizedBox(width: 20),
-                  Text(
-                    "분리배출 방법",
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      fontFamily: 'gmarket',
-                      fontWeight: FontWeight.w500,
-                      fontSize: 15,
-                      color: mainColor_green,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 8),
-              Text(
-                "- 빈 용기는 밥알이 남아있지 않게 깨끗이 세척한 후\n   플라스틱으로 배출해요.\n- 비닐 뚜껑은 떼어내서 비닐류로 배출해요.",
-                style: subtitle1(),
-              ),
-              const SizedBox(height: 20),
-              item.isCollected
-                  ? Column(
-                      children: [
-                        const Row(
-                          children: [
-                            SizedBox(width: 20),
-                            Text(
-                              "알아두면 좋은 점",
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                fontFamily: 'gmarket',
-                                fontWeight: FontWeight.w500,
-                                fontSize: 15,
-                                color: mainColor_green,
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 8),
-                        Text(
-                          "즉석밥 용기에 쓰이는 플라스틱 OTHER는\n내열성을 높이기 위해 여러 재질을 혼합한 복합\n플라스틱으로 제품 종류별로 수거해 별도의\n재활용 공정을 거치지 않는 이상 재활용이 불가능해요.\n그래서 재활용장에서 선별되지 못하고 대부분\n일반쓰레기로 폐기(소각 또는 매립)된다고 합니다.",
-                          style: subtitle1(),
-                        ),
-                      ],
-                    )
-                  : const SizedBox(height: 115),
-            ],
-          ),
+          ],
         ),
         actions: [
           Stack(
